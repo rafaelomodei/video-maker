4dd9d6013b58b02efb4b284b32e78731107f91dd
Rafael Omodei <omodei@alunos.utfpr.edu.br>
2021-01-05 17:17:01 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 /* 
 ### Instalar Dependência
- $ npm install readline-sync
+ $ npm i readline-sync
+ $ npm i algorithmia
+ $ npm i sbd
+ $ npm i watson-developer-cloud
+ $ npm i googleapis
 */
+
 
 const state = require('./robots/state.js')
 
 const robots = {
     //userInput: require('./robots/user-input.js'),
-    input: require('./robots/inputs.js'),
+    input: require('./robots/input.js'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 }
 
 async function start(){
 
-    //Robôs
+    Robôs
     robots.input()
     await robots.text()
+    await robots.image()
     
     const content = robots.state.load()
     //imprime que nem o log porem de uma forma mais bonita
